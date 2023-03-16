f9e557032b706ce640308a3a73cf05960a66a6e9
MukhammedAli <muhammedalikhamzayev@gmail.com>
2023-03-16 21:11:33 +0600

    Sign In view is completed

---
+++ SDU News/Controllers/RegistrationControllers/SignUpViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpViewController.swift
+//  SDU News
+//
+//  Created by  Mukhammed Ali Khamzayev on 16.03.2023.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
